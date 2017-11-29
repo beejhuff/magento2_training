52600ec12bc5f6fc5ed46aaaf3490a90e6aaec23
Jonathan Holt <j.holt@ctidigital.com>
2017-11-29 15:21:19 +0000

    Simple UI Knockout Component

--- app/code/Pulsestorm/SimpleUiComponent/Model/ValidationState.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Pulsestorm\SimpleUiComponent\Model;
-class ValidationState extends \Magento\Framework\App\Arguments\ValidationState
-{
-    public function isValidationRequired()
-    {        
-        return false;
-    }
-}
